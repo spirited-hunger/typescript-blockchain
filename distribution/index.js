30b7d7f0a0aa53fee9c145482e4a918da677c78d
Spirited Hunger <sjkang920921@gmail.com>
2021-08-21 13:58:47 +0900

    upgraded typescript setup

---
+++ distribution/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Sung", age = 30, gender = "male";
+const sayHi = (name, age, gender) => {
+    return `Hi, ${name}, you are ${age} and you are a ${gender}`;
+};
+console.log(sayHi("Catherine", 27, "female"));
+//# sourceMappingURL=index.js.map
