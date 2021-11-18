8a9d6b8e65a39d02a2e908d302b30ca687f33392
Spirited Hunger <sjkang920921@gmail.com>
2021-11-18 23:38:21 +0900

    feat:block

---
+++ src/blockchain.ts
@@ -0,0 +1,26 @@
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ){
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
+
+const genesisBlock:Block = new Block(0, "2020202020202", "", "RandomData", 1234);
+
+let blockchain: [Block] = [genesisBlock];
+
+console.log(blockchain);
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,44 @@
+// * sample code of interface
 
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+
+// const person = {
+//   name: "Sung",
+//   age: 30,
+//   gender:  "male"
+// }
+
+
+// * sample code of class
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Sung",
-  age: 30,
-  gender:  "male"
-}
+const sung = new Human("Sung", 30, "male");
+
+
+// * sample code of function
+// $ tsc-watch src/index.ts
 
 const sayHi = (person: Human, message?:string): string => {
   return `Hi, ${person.name}, you are ${person.age} and you are a ${person.gender}. ${message ? message : ""}`;
 }
 
-console.log(sayHi(person));
+console.log(sayHi(sung));
+
+
+// just write this to compile
 
 export {};
